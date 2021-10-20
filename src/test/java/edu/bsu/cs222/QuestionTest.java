f11b5bc48aefed864a6e735f856c99a38190e963
wjgibson <wjgibson@bsu.edu>
2021-10-20 16:07:30 -0400

    Added testForIncorrectAnswerResponse

--- src/test/java/edu/bsu/cs222/QuestionTest.java
+++ src/test/java/edu/bsu/cs222/QuestionTest.java
@@ -15,5 +15,15 @@ public class QuestionTest {
         Assertions.assertEquals("Blue",testQuestion.getCorrectAnswer());
     }
 
+    @Test
+    public void testForIncorrectAnswerResponse(){
+        Question testQuestion = new Question("What is the color of the sky?",
+                "Blue" ,
+                "Red" ,
+                "Green" ,
+                "Brown");
+        Assertions.assertEquals("Red",testQuestion.getIncorrectAnswers()[0]);
+    }
+
 
 }
