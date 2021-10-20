993f31ba917ec4cdc56843797eddaf03e287d609
wjgibson <wjgibson@bsu.edu>
2021-10-20 16:01:27 -0400

    Added testForCorrectAnswer

--- src/test/java/edu/bsu/cs222/QuestionTest.java
+++ src/test/java/edu/bsu/cs222/QuestionTest.java
@@ -1,9 +1,19 @@
 package edu.bsu.cs222;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class QuestionTest {
 
     @Test
+    public void testForCorrectAnswerResponse(){
+        Question testQuestion = new Question("What is the color of the sky?",
+                "Blue" ,
+                "Red" ,
+                "Green" ,
+                "Brown");
+        Assertions.assertEquals("Blue",testQuestion.getCorrectAnswer());
+    }
+
 
 }
