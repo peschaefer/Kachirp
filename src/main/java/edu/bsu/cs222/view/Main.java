0e891577e4933d732116a5d2e5dd218827d97f59
wtwithers2020 <wtwithers2020@gmail.com>
2021-10-24 16:43:59 -0400

    refactor Main
    Main now accepts user input, and checks to see if the user input is correct or incorrect. Prints a message accordingly.

--- src/main/java/edu/bsu/cs222/view/Main.java
+++ src/main/java/edu/bsu/cs222/view/Main.java
@@ -9,20 +9,29 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        UserInput userInput = new UserInput();
         TriviaParser parser = new TriviaParser();
         TriviaAPIConnector connector = new TriviaAPIConnector();
         String triviaData = connector.connectToApi("https://api.trivia.willfry.co.uk/questions?limit=5");
         parser.addQuestions(triviaData, 5);
         ArrayList<Question> questionArrayList = parser.getQuestionArrayList();
+
         for(int questionIndex = 0; questionIndex < 5; questionIndex++) {
             displayQuestionInformation(questionArrayList.get(questionIndex));
+            int userAnswer = Integer.parseInt(userInput.getInput());
+            if (userAnswer-1 == questionArrayList.get(questionIndex).getCorrectAnswerIndex()){
+                System.out.println("\nGood Job! You got it right!\n");
+            }
+            else{
+                System.out.println("\nYou are a failure, and you should feel bad.\n");
+            }
         }
     }
 
     private static void displayQuestionInformation(Question question){
         System.out.println(question.getQuestionText()+"\n");
         for(int index = 0;index < 4; index++) {
-            System.out.println(question.getAnswers()[index]);
+            System.out.println(index+1+ ". " + question.getAnswers()[index]);
         }
         System.out.println();
     }
--- src/main/java/edu/bsu/cs222/view/UserInput.java
+++ src/main/java/edu/bsu/cs222/view/UserInput.java
@@ -3,7 +3,7 @@ package edu.bsu.cs222.view;
 import java.util.Scanner;
 
 public class UserInput {
-    public String inputString(){
+    public String getInput(){
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
