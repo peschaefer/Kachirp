20afe191dd696955669cd0041c389b2a7a1d5f86
peschaefer <peschaefer@bsu.edu>
2021-10-26 11:20:42 -0400

    Add QuestionFormatter and QuestionFormatterTest and Refactor Main
    QuestionFormatter has a method that formats the questions for printing in the console.
    
    QuestionFormatterTest has a method called formatQuestionTest() that ensure the questions are formatted correctly.
    
    Main was refactored to no longer do anything but run a console class that needs to be written.  This eliminates static calls as well as helps with MVS.

--- src/main/java/edu/bsu/cs222/view/Main.java
+++ src/main/java/edu/bsu/cs222/view/Main.java
@@ -9,52 +9,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        UserInput userInput = new UserInput();
-        TriviaParser parser = new TriviaParser();
-        TriviaAPIConnector connector = new TriviaAPIConnector();
-
-        String triviaData = connector.connectToApi("https://api.trivia.willfry.co.uk/questions?limit=5");
-        checkForValidConnection(triviaData);
-        parser.addQuestions(triviaData, 5);
-        ArrayList<Question> questionArrayList = parser.getQuestionArrayList();
-
-        int questionIndex = 0;
-        while(questionIndex < 5) {
-            int userAnswer;
-            displayQuestionInformation(questionArrayList.get(questionIndex));
-            try{
-                userAnswer = Integer.parseInt(userInput.getInput());
-            }catch(NumberFormatException e){
-                System.out.println("\nThat is not a valid response.\n");
-                continue;
-            }
-            checkAnswer(userAnswer,questionArrayList.get(questionIndex).getCorrectAnswerIndex());
-            questionIndex++;
-        }
-    }
-
-    private static void checkForValidConnection(String triviaData) {
-        if (triviaData.isEmpty()){
-            System.err.println("A network error has occurred");
-            System.exit(0);
-        }
-    }
-
-    private static void displayQuestionInformation(Question question){
-        System.out.println(question.getQuestionText()+"\n");
-        for(int index = 0;index < 4; index++) {
-            System.out.println(index + 1 + ". " + question.getAnswers()[index]);
-        }
-        System.out.println();
-    }
-    private static void checkAnswer(int userAnswer, int correctAnswerIndex){
-        //Since the index is numbered 0-3 and the user input is 1-4, it must be decremented to check correctness
-        userAnswer -= 1;
-        if(userAnswer == correctAnswerIndex){
-            System.out.println("\nGood Job! You got it right!\n");
-        }
-        else{
-            System.out.println("\nYou are a failure, and you should feel bad.\n");
-        }
+        Console game = new Console();
+        game.runConsole();
     }
 }
--- src/test/java/edu/bsu/cs222/QuestionTest.java
+++ src/test/java/edu/bsu/cs222/QuestionTest.java
@@ -7,21 +7,21 @@ import org.junit.jupiter.api.Test;
 public class QuestionTest {
     String[] incorrectAnswers = new String[]{"Red","Green","Blue"};
     @Test
-    public void testForCorrectAnswerResponse() {
+    public void correctAnswerResponseTest() {
         Question testQuestion = new Question("What is the color of the sky?", "Blue", incorrectAnswers);
         int index = testQuestion.getCorrectAnswerIndex();
         Assertions.assertEquals("Blue", testQuestion.getAnswers()[index]);
     }
 
     @Test
-    public void testForIncorrectAnswerResponse(){
+    public void incorrectAnswerResponseTest(){
         Question testQuestion = new Question("What is the color of the sky?", "Blue", incorrectAnswers);
         int index = testQuestion.getCorrectAnswerIndex();
         Assertions.assertNotEquals("Red", testQuestion.getAnswers()[index]);
     }
 
     @Test
-    public void testForQuestionText(){
+    public void questionTextTest(){
         Question testQuestion = new Question("What is the color of the sky?", "Blue", incorrectAnswers);
         Assertions.assertEquals("What is the color of the sky?",testQuestion.getQuestionText());
     }
