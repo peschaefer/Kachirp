a04e388223ed631ab107b7da6ceed67df98693c1
peschaefer <peschaefer@bsu.edu>
2021-10-26 11:23:16 -0400

    Add Console and Refactor Main
    Spacing in Main was bad, this was fixed.
    
    Console is a new class that does what Main did previously.  This class handles taking user input and printing to the console.

---
+++ src/main/java/edu/bsu/cs222/view/Console.java
@@ -0,0 +1,59 @@
+package edu.bsu.cs222.view;
+
+import edu.bsu.cs222.model.Question;
+import edu.bsu.cs222.model.TriviaAPIConnector;
+import edu.bsu.cs222.model.TriviaParser;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Console {
+    QuestionFormatter formatter = new QuestionFormatter();
+
+    public void runConsole() throws IOException {
+
+        UserInput userInput = new UserInput();
+        TriviaParser parser = new TriviaParser();
+        TriviaAPIConnector connector = new TriviaAPIConnector();
+
+        String triviaData = connector.connectToApi("https://api.trivia.willfry.co.uk/questions?limit=5");
+        checkForValidConnection(triviaData);
+        parser.addQuestions(triviaData, 5);
+        ArrayList<Question> questionArrayList = parser.getQuestionArrayList();
+
+        int questionIndex = 0;
+        while (questionIndex < 5) {
+            int userAnswer;
+            displayQuestionInformation(questionArrayList.get(questionIndex));
+            try {
+                userAnswer = Integer.parseInt(userInput.getInput());
+            } catch (NumberFormatException e) {
+                System.out.println("\nThat is not a valid response.\n");
+                continue;
+            }
+            checkAnswer(userAnswer, questionArrayList.get(questionIndex).getCorrectAnswerIndex());
+            questionIndex++;
+        }
+    }
+
+    private void checkForValidConnection (String triviaData){
+        if (triviaData.isEmpty()) {
+            System.err.println("A network error has occurred");
+            System.exit(0);
+        }
+    }
+
+    private void displayQuestionInformation (Question question){
+        System.out.println(formatter.formatQuestion(question));
+    }
+
+    private void checkAnswer ( int userAnswer, int correctAnswerIndex){
+        //Since the index is numbered 0-3 and the user input is 1-4, it must be decremented to check correctness
+        userAnswer -= 1;
+        if (userAnswer == correctAnswerIndex) {
+            System.out.println("\nGood Job! You got it right!\n");
+        } else {
+            System.out.println("\nYou are a failure, and you should feel bad.\n");
+        }
+    }
+}
