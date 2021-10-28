38235c9177918c9b84dfea9ff9301c069a86711a
wtwithers2020 <wtwithers2020@gmail.com>
2021-10-28 16:30:13 -0400

    Refactor URLBuilder
    Removed an extraneous if statement

--- src/main/java/edu/bsu/cs222/model/URLBuilder.java
+++ src/main/java/edu/bsu/cs222/model/URLBuilder.java
@@ -3,9 +3,6 @@ package edu.bsu.cs222.model;
 public class URLBuilder {
     public String buildURL(String categories, int numberOfQuestions){
         categories = categories.replace(' ','_');
-        if(categories.isEmpty()){
-            return "https://api.trivia.willfry.co.uk/questions?limit="+numberOfQuestions;
-        }
 
         return "https://api.trivia.willfry.co.uk/questions?categories="+categories+"&limit="+numberOfQuestions;
     }
