7c46087c7832a45f158c23c4fdbfa9a97b5ffb44
Blade0810 <blade8030@gmail.com>
2021-10-31 13:09:04 -0400

    Add QuestionBankWriter and writeNewQuestionBank and remove model/view packages.
    QuestionBankWriter uses writeNewQuestionBank() to create a new file in a resource directory containing user created question banks.
    
    model/view package separation was removed due to unnecessary clarification

--- src/main/java/edu/bsu/cs222/Main.java
+++ src/main/java/edu/bsu/cs222/Main.java
@@ -1,12 +1,12 @@
 package edu.bsu.cs222;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+//        CustomQuestionCreator cretor = new CustomQuestionCreator();
+//        QuestionBankWriter writer = new QuestionBankWriter();
+//        writer.writeNewQuestionBank(cretor.createCustomQuestions(),"testBank");
         Console game = new Console();
         game.runConsole();
     }
---
+++ src/main/java/edu/bsu/cs222/QuestionBankWriter.java
@@ -0,0 +1,24 @@
+package edu.bsu.cs222;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class QuestionBankWriter {
+    public void writeNewQuestionBank(ArrayList<Question> questions, String questionBankName) throws IOException {
+
+        File outputFile = new File("src/main/java/QuestionBanks/"+questionBankName+".json");
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(questions);
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+        writer.write(json);
+
+        writer.close();
+    }
+}
