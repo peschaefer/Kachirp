ffbe56f02066775e51bf98734db34c8b8b1dcc5d
wjgibson <wjgibson@bsu.edu>
2021-11-14 11:05:20 -0500

    Add functionality to shuffle the order of custom questions.
    Collections.shuffle() allows things to be easily randomized.

--- src/main/java/edu/bsu/cs222/Parser.java
+++ src/main/java/edu/bsu/cs222/Parser.java
@@ -1,6 +1,7 @@
 package edu.bsu.cs222;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 abstract class Parser {
     ArrayList<Question> questionArrayList = new ArrayList<>();
@@ -14,6 +15,7 @@ abstract class Parser {
     }
 
     public ArrayList<Question> getQuestionArrayList(){
+        Collections.shuffle(questionArrayList);
         return questionArrayList;
     }
 }
