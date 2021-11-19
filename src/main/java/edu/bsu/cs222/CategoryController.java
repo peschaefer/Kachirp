0e3ed4e3cdd92e0891550c611fa6e102d7152794
wjgibson <wjgibson@bsu.edu>
2021-11-19 16:52:34 -0500

    Add CategoryController.java
    CategoryController.java uses the checkboxes in CategorySelect.fxml to populate an array list. The method within CategoryController.java populates an arrayList of categories and returns it to be used for later.

---
+++ src/main/java/edu/bsu/cs222/CategoryController.java
@@ -0,0 +1,62 @@
+package edu.bsu.cs222;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
+
+import java.util.ArrayList;
+
+public class CategoryController {
+    ArrayList<String> guiCategorySelections = new ArrayList<>();
+
+    ArrayList<CheckBox> checkBoxes = new ArrayList<>();
+
+    @FXML
+    private TextArea tempTextArea;
+    @FXML
+    private CheckBox foodAndDrinkBox;
+    @FXML
+    private CheckBox geographyBox;
+    @FXML
+    private CheckBox generalKnowledgeBox;
+    @FXML
+    private CheckBox historyBox;
+    @FXML
+    private CheckBox artAndLiteratureBox;
+    @FXML
+    private CheckBox scienceBox;
+    @FXML
+    private CheckBox societyAndCultureBox;
+    @FXML
+    private CheckBox sportsAndLeisureBox;
+    @FXML
+    private CheckBox musicBox;
+    @FXML
+    private CheckBox moviesBox;
+
+    private void populateCheckboxArrayList(){
+        //Send a GET request to the FXML document for all fxid's (later)
+        checkBoxes.add(foodAndDrinkBox);
+        checkBoxes.add(geographyBox);
+        checkBoxes.add(generalKnowledgeBox);
+        checkBoxes.add(historyBox);
+        checkBoxes.add(artAndLiteratureBox);
+        checkBoxes.add(scienceBox);
+        checkBoxes.add(societyAndCultureBox);
+        checkBoxes.add(sportsAndLeisureBox);
+        checkBoxes.add(musicBox);
+        checkBoxes.add(moviesBox);
+    }
+
+
+    public ArrayList<String> populateCategoryArrayList(){
+        populateCheckboxArrayList();
+        for (CheckBox box:checkBoxes) {
+            if(box.isSelected()){
+                guiCategorySelections.add(box.getText());
+            }
+        }
+        tempTextArea.setText(guiCategorySelections.toString());
+        return guiCategorySelections;
+    }
+}
