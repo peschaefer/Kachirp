d752d7c9458742b8bfd0234f6ac6b8d3a8d6c3bb
peschaefer <peschaefer@bsu.edu>
2021-11-21 12:16:12 -0500

    Start adding GUI question prompting and functionality
    Added bare bones functionality to CustomGame.fxml that takes uses a default question with buttons that can tell if an answer is right or wrong.

---
+++ src/main/java/edu/bsu/cs222/GameController.java
@@ -0,0 +1,57 @@
+package edu.bsu.cs222;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class GameController {
+
+    public Label questionTextLabel;
+    public Button option1;
+    public Button option2;
+    public Button option3;
+    public Button option4;
+    public Label answerResponse;
+    public int answerChoice;
+    public Question question;
+
+    public void setQuestionText(String text){
+        questionTextLabel.setText(text);
+    }
+
+    public void setQuestion(Question question){
+        this.question = question;
+    }
+
+    public void setAnswerChoices(String[] answers){
+        option1.setText(answers[0]);
+        option2.setText(answers[1]);
+        option3.setText(answers[2]);
+        option4.setText(answers[3]);
+    }
+
+    public void checkAnswer(int answerChoice){
+        if(question.getCorrectAnswerIndex()+1 == answerChoice){
+            answerResponse.setText("Correct!");
+        }
+        else{
+            answerResponse.setText("Wrong!");
+        }
+    }
+
+    public void setAnswerChoice1() {
+        answerChoice = 1;
+        checkAnswer(1);
+    }
+    public void setAnswerChoice2() {
+        answerChoice = 1;
+        checkAnswer(2);
+    }
+    public void setAnswerChoice3() {
+        answerChoice = 1;
+        checkAnswer(3);
+    }
+    public void setAnswerChoice4() {
+        answerChoice = 1;
+        checkAnswer(4);
+    }
+}
--- src/main/java/edu/bsu/cs222/SceneController.java
+++ src/main/java/edu/bsu/cs222/SceneController.java
@@ -7,15 +7,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
+import javax.imageio.IIOParam;
+import javax.swing.*;
 import java.io.IOException;
 import java.util.Objects;
 
 public class SceneController extends Application {
+
+    Question question = new Question("What is my middle name?","Edward",new String[]{"Richard","James","Niall"});
     private Scene scene;
     private Parent root;
-    private Stage stage;
 
     @Override
     public void start(Stage primaryStage){
@@ -34,51 +39,50 @@ public class SceneController extends Application {
     }
 
     //VanillaGame and CustomGame should be one scene
-    public void switchToCustomGame(javafx.event.ActionEvent event) {
+    public void switchToCustomGame(javafx.event.ActionEvent event) throws IOException {
         try {
-            root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("CustomGame.fxml")));
-        } catch (IOException ioException) {
+            FXMLLoader loader  = new FXMLLoader(Objects.requireNonNull(getClass().getClassLoader().getResource("CustomGame.fxml")));
+
+            root = loader.load();
+
+            GameController gameController = loader.getController();
+
+            gameController.setQuestion(question);
+
+            gameController.setQuestionText(question.getQuestionText());
+            gameController.setAnswerChoices(question.getAnswers());
+        }
+        catch (IOException ioException) {
             throw new RuntimeException(ioException);
         }
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
     }
     public void switchToQuestionCreator(javafx.event.ActionEvent event) {
-        try {
-            root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("QuestionCreator.fxml")));
-        } catch (IOException ioException) {
-            throw new RuntimeException(ioException);
-        }
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        switchScene(event,"QuestionCreator.fxml");
     }
-
     //VanillaGame and CustomGame should be one scene
     public void switchToVanillaGame(javafx.event.ActionEvent event) {
+        switchScene(event,"VanillaGame.fxml");
+    }
+
+    public void switchScene(ActionEvent event, String sceneName){
         try {
-            root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("VanillaGame.fxml")));
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource(sceneName)));
         } catch (IOException ioException) {
             throw new RuntimeException(ioException);
         }
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
     public void switchToCategorySelection(javafx.event.ActionEvent event) {
-        try {
-            root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("CategorySelect.fxml")));
-        } catch (IOException ioException) {
-            throw new RuntimeException(ioException);
-        }
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        switchScene(event,"CategorySelect.fxml");
     }
 }
