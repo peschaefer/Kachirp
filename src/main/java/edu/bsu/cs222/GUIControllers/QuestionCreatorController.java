e6e7db8969971448840caad8eb2cf725016a0016
peschaefer <peschaefer@bsu.edu>
2021-11-24 18:53:04 -0500

    Refactor QuestionCreatorController
    TextFields are cleared after each question submitted

--- src/main/java/edu/bsu/cs222/GUIControllers/QuestionCreatorController.java
+++ src/main/java/edu/bsu/cs222/GUIControllers/QuestionCreatorController.java
@@ -1,7 +1,6 @@
 package edu.bsu.cs222.GUIControllers;
 
 import edu.bsu.cs222.Question;
-import edu.bsu.cs222.QuestionBankCreator;
 import edu.bsu.cs222.QuestionBankWriter;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
@@ -21,16 +20,16 @@ public class QuestionCreatorController {
     QuestionBankWriter writer = new QuestionBankWriter();
 
     Main main;
-    QuestionBankCreator creator = new QuestionBankCreator();
+    //QuestionBankCreator creator = new QuestionBankCreator();
     ArrayList<Question> currentQuestionList= new ArrayList<>();
 
     public void setMain(Main main) {
         this.main = main;
     }
 
-    public void checkForOverwrite(ActionEvent event) {
-        System.out.println(creator.bankPresent(questionBankName.getText() + ".json"));
-    }
+//    public void checkForOverwrite(ActionEvent event) {
+//        System.out.println(creator.bankPresent(questionBankName.getText() + ".json"));
+//    }
 
 
     public void addQuestion() {
@@ -39,10 +38,19 @@ public class QuestionCreatorController {
         String[] incorrectAnswers = new String[]{incorrectField1.getText(),incorrectField2.getText(),incorrectField3.getText()};
         Question question = new Question(questionText,correctAnswer,incorrectAnswers);
         currentQuestionList.add(question);
+        clearTextFields();
     }
 
     public void submitBank(ActionEvent event) throws IOException {
         writer.writeNewQuestionBank(currentQuestionList,questionBankName.getText() + ".json");
         main.switchToMainMenu(event);
+    }
+
+    private void clearTextFields(){
+        questionTextField.clear();
+        correctAnswerField.clear();
+        incorrectField1.clear();
+        incorrectField2.clear();
+        incorrectField3.clear();
     }
 }
