1fe8fcab95cf79e5fb4e3dc31ab3b2657ae49a55
Blade0810 <blade8030@gmail.com>
2021-12-01 16:48:43 -0500

    change layout of fxml files

--- src/main/java/edu/bsu/cs222/GUIControllers/Main.java
+++ src/main/java/edu/bsu/cs222/GUIControllers/Main.java
@@ -26,6 +26,7 @@ public class Main extends Application {
         }
         scene = new Scene(root);
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
