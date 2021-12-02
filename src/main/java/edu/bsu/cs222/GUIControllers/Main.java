44e5df781f76c836d8e56a6de6ad65b04ed8b435
wjgibson <wjgibson@bsu.edu>
2021-12-02 11:14:09 -0500

    change color of slider.
    The slider is now implemented into the color scheme.

--- src/main/java/edu/bsu/cs222/GUIControllers/Main.java
+++ src/main/java/edu/bsu/cs222/GUIControllers/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Objects;
